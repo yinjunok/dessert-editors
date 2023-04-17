151ec1b262380313dfdf38e7368d656352f48520
root <yinjunok@foxmail.com>
2023-04-17 16:26:33 +0800

    删除不需要的代码

--- packages/base-slate/src/SlateEditor/utils.ts
+++ packages/base-slate/src/SlateEditor/utils.ts
@@ -1,5 +1,4 @@
 import { Descendant, Node } from 'slate'
-import type { BaseElement, ElementType } from './types'
 
 export const serialize = (val: Descendant[]) => {
   return val.map(n => Node.string(n)).join('\n')
@@ -9,10 +8,3 @@ export const deserialize  = (str: string) => {
   return str.split('\n').map(line => ({ type: 'paragraph', children: [{ text: line }] }))
 }
 
-export const createBaseElement = (params?: Partial<Omit<BaseElement, 'type'>>): Omit<BaseElement, 'type'> => ({
-  isInline: false,
-  isVoid: false,
-  markableVoid: true,
-  children: [{ text: '' }],
-  ...params
-})
